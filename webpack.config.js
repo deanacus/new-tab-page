ae54cb3e8c6e00b1a8079c059b30f03a83a73909
Dean Harris <dean@harris.tc>
2019-01-02 15:43:34 +1000

    Update devServer settings and add a better index.html file

--- webpack.config.js
+++ webpack.config.js
@@ -40,22 +40,7 @@ const config = {
 	},
 	devServer: {
 		contentBase: './public',
-		stats: {
-			colors: true,
-			hash: false,
-			version: false,
-			timings: false,
-			assets: false,
-			chunks: false,
-			modules: false,
-			reasons: false,
-			children: false,
-			source: false,
-			errors: false,
-			errorDetails: false,
-			warnings: false,
-			publicPath: false,
-		  },
+		quiet: true,
 	},
 	plugins: [
 		new FriendlyErrorsWebpackPlugin(),
